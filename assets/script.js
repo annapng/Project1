b004d27561119da5cc3aa9e11cffb9a37735f2b9
EarlofSmith <r1smith3333@gmail.com>
2022-11-22 18:54:53 -0700

    got button to return proper value

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,22 @@
-class Recipes {
 
-}
 
 class recipeResults {
     async fetchRecipe(input) {
 
 const key = 'c532660435c5437ea9550a5436d094b0'
 
-<<<<<<< HEAD
-search= await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${key}&query=${input}`);
-=======
-search = awaitfetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${key}&query=${input}`);
->>>>>>> 073e1d2c99d054f77b6a8cb5c253334ffcff3cd1
+search = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${key}&query=${input}`);
 console.log(search);
 var storedRecipe = await search.json();
 console.log(storedRecipe);
 return storedRecipe;
 }
+}    
+class showResults{
     constructor(){
         this.container = document.getElementById("search-results");
     }
+
     displaySearch(data) {
         console.log(data);
         this.container.innerHTML += `
@@ -32,23 +29,19 @@ return storedRecipe;
         </ul>
         `
     }
-}
-var R = new Recipes();
+};
+var SR = new showResults();
 var RR = new recipeResults
 var search = document.getElementById("Search-bar");
 var button = document.getElementById("search-btn");
 button.addEventListener("click", () => {
     const choice= search.value;
-<<<<<<< HEAD
     search.value="";
     RR.fetchRecipe(choice).then((data) =>{
-=======
-    search.value = "";
-    R.fetchRecipe(choice).then((data) =>{
->>>>>>> 073e1d2c99d054f77b6a8cb5c253334ffcff3cd1
         console.log(data);
+        SR.displaySearch(data);
     })
-    RR.displaySearch(choice).then((data) => {
-        console.log(data);
-    })
+    // .then(() => {
+    //     console.log(data);
+    // })
 })
