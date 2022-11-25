e73f4bef4ba892ab6a7c71694c62cf53d82d97e0
helenhosse <helenhosse@gmail.com>
2022-11-25 10:35:08 -0700

    2nd page changes

--- assets/secondscript.js
+++ assets/secondscript.js
@@ -1,4 +1,51 @@
-class Recipes {
+const recipeContainer = document.querySelector("#recipe-container")
+
+function loadRecipes(type = "products"){
+    const baseURL = `https://api.spoonacular.com/food/products/search?query=yogurt&apiKey=c532660435c5437ea9550a5436d094b0`; 
+    const url = baseURL + '&q=${type}'; 
+    fetch(url)
+        .then(res => res.json())
+        .then((data) => renderRecipies(data))
+        .then(data => console.log(data));
+        try {
+            
+        } catch (error) {
+            
+        }((error) => console.log(error));
+}
+loadRecipes(type = "products");
+
+var renderRecipies = (recipeList=[]) => {
+    recipeList.forEach((recipeObj) => {
+        const { 
+            title: recipeTitle, 
+            ingredientLines: ingredientsList,
+            image: recipeImage,
+    } = recipeObj.recipe;
+        const htmlStr = `<div class="recipe"> 
+        <div id="recTitle">${recipeTitle}</div>
+        <div id="recipe-img">;
+          <img src="${recipeImage}" alt="photo of recipe">
+        </div>
+        <div id="ingList">${ingredientsList}</div>
+        <div id="actRecipe">Actual Recipe
+          <ul>
+            <li>Step 1</li>
+            <li>Step 2</li>
+            <li>Step 3</li>
+            <li>Step 4</li>
+            <li>Step 5</li>
+          </ul>
+        </div>
+      </div>
+      </div>`;
+      recipeContainer.insertAdjacentHRML("beforeend", htmlStr);
+    });
+}
+
+
+
+/*class Recipes {
     async fetchRecipe(input) {
     
     const key = 'c532660435c5437ea9550a5436d094b0'
@@ -22,3 +69,4 @@ button.addEventListener("click", () => {
         console.log(data);
     })
 })
+*/
