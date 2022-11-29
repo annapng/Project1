0e081a289879a69d0f97011c957ed8ee6b8c3c9b
wjg97 <113846649+wjg97@users.noreply.github.com>
2022-11-28 20:29:30 -0700

    getting weather api going

--- assets/secondscript.js
+++ assets/secondscript.js
@@ -83,11 +83,43 @@ class randomRecipe {
         localStorage.setItem('recipe', JSON.stringify(data));
         location.reload();
         })
+    });
+
+
+
+
+
+
+const weatherkey = 'icI2CTNjyNO2wVzp28G48OGkp40Eim92'
+
+const getWeather = async (id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+    
+    const response = await fetch(base + query);
+    const data = await response.json();
+    
+    return data[0];
+    console.log(data);
+};
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+    
+    const response = await fetch(base + query);
+    const data = await response.json();
+    console.log(data[0]);
+};
+    
+getCity('cheyenne')
+    .then(data => {
+        return getWeather(data.Key)
+    }).then(data => {
+        console.log(data);
     })
-
-
-
-
+    .catch(err => console.log(err));
+    
+getWeather('331604');   
 
 
 /*function loadRecipes(type = "products"){
@@ -160,3 +192,6 @@ button.addEventListener("click", () => {
     })
 })
 */
+
+
+ 
