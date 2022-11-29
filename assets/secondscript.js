c7b48d2d946fb905162dabd018d3bf04cb5f351c
helenhosse <helenhosse@gmail.com>
2022-11-28 20:13:58 -0700

    plz ignore

--- assets/secondscript.js
+++ assets/secondscript.js
@@ -85,7 +85,31 @@ class randomRecipe {
         })
     })
 
-
+    class Recipes {
+        async fetchRecipe(input) {
+        
+        const key = 'c532660435c5437ea9550a5436d094b0'
+        
+        search = awaitfetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${key}&query=${input}`);
+        console.log(search);
+        var storedRecipe = await search.json();
+        console.log(storedRecipe);
+        return storedRecipe;
+        }
+        }
+    
+    
+    var R = new Recipes();
+    var search = document.getElementById("try-again");
+    
+    button.addEventListener("click", () => {
+        const choice = search.value;
+        search.value = "";
+        R.fetchRecipe(choice).then((data) => {
+            console.log(data);
+        })
+    })
+    
 
 
 
@@ -135,7 +159,7 @@ var renderRecipies = (recipeList=[]) => {
 
 
 
-/*class Recipes {
+class Recipes {
     async fetchRecipe(input) {
     
     const key = 'c532660435c5437ea9550a5436d094b0'
@@ -159,4 +183,4 @@ button.addEventListener("click", () => {
         console.log(data);
     })
 })
-*/
+
