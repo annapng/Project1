655d4cc40bb867af4a930c9ae6a36e6f7c48c11e
annapng <64225221+annapng@users.noreply.github.com>
2022-11-29 17:05:20 -0700

    search results working properly!!

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
-// new emergency api key:
-const key = '4f01a6d633eb4639b8a8c4c8417804df';
+const key = '7241575ebfdb41ba8e306367db250539';
+// const key = '8dfc602f94974cecb5169da744c24a2b';
+// const key = '06cbe8b372eb41cc8ba05702e3523e36';
 
 
 function App() {
@@ -10,12 +11,6 @@ function App() {
 
 class recipeResults {
     async fetchRecipe(input) {
-
-
-// added a second key we can use if we get to more then the set number of calls on the first key. just comment one
-// const key = 'c532660435c5437ea9550a5436d094b0'
-//  const key ='802a019a602c480da05b17676eeb3ce3'
-//  const key = "8848face408443bda493657897ec7fac"
 
 
 let checkboxes = document.querySelectorAll("input[type='checkbox']:checked");
@@ -56,24 +51,121 @@ class showResults{
         console.log(data);
 
         if (data.results[0] !== 'undefined'){
-            this.container.innerHTML += 
-            `<ul class="search">
-                <button type=button id= "${data.results[0].title}" class="results">${data.results[0].title} <img src=${data.results[0].image} </button>
-            </div> `
-            var btn0 = document.getElementById(`${data.results[0].title}`);
-            btn0.addEventListener("click",() => {
+            this.container.insertAdjacentHTML(`beforeend`, `<ul class="search">
+            <button type=button id= "btn0" class="results">${data.results[0].title} <img src=${data.results[0].image}> </button> </div> `); 
+
+            document.getElementById("btn0").addEventListener("click",() => {
                 localStorage.setItem(`recipe`,JSON.stringify(data));
                 localStorage.setItem(`current-recipe`,JSON.stringify(0)); 
-                document.location.href = "./second-page.html"
-            })
+                document.location.href = "./second-page.html";
+            }) 
         } else {null;}
 
         if (data.results[1] !== 'undefined'){
+            this.container.insertAdjacentHTML(`beforeend`, `<ul class="search">
+            <button type=button id= "btn1" class="results">${data.results[1].title} <img src=${data.results[1].image}> </button> </div> `); 
+
+            document.getElementById("btn1").addEventListener("click",() => {
+                localStorage.setItem(`recipe`,JSON.stringify(data));
+                localStorage.setItem(`current-recipe`,JSON.stringify(1)); 
+                document.location.href = "./second-page.html";
+            }) 
+        } else {null;}
+
+        if (data.results[2] !== 'undefined'){
+            this.container.insertAdjacentHTML(`beforeend`, `<ul class="search">
+            <button type=button id= "btn2" class="results">${data.results[2].title} <img src=${data.results[2].image}> </button> </div> `); 
+
+            document.getElementById("btn2").addEventListener("click",() => {
+                localStorage.setItem(`recipe`,JSON.stringify(data));
+                localStorage.setItem(`current-recipe`,JSON.stringify(2)); 
+                document.location.href = "./second-page.html";
+            }) 
+        } else {null;}
+
+        if (data.results[3] !== 'undefined'){
+            this.container.insertAdjacentHTML(`beforeend`, `<ul class="search">
+            <button type=button id= "btn3" class="results">${data.results[3].title} <img src=${data.results[3].image}> </button> </div> `); 
+
+            document.getElementById("btn3").addEventListener("click",() => {
+                localStorage.setItem(`recipe`,JSON.stringify(data));
+                localStorage.setItem(`current-recipe`,JSON.stringify(3)); 
+                document.location.href = "./second-page.html";
+            }) 
+        } else {null;}
+
+        if (data.results[4] !== 'undefined'){
+            this.container.insertAdjacentHTML(`beforeend`, `<ul class="search">
+            <button type=button id= "btn4" class="results">${data.results[4].title} <img src=${data.results[4].image}> </button> </div> `); 
+
+            document.getElementById("btn4").addEventListener("click",() => {
+                localStorage.setItem(`recipe`,JSON.stringify(data));
+                localStorage.setItem(`current-recipe`,JSON.stringify(4)); 
+                document.location.href = "./second-page.html";
+            }) 
+        } else {null;}
+
+        if (data.results[5] !== 'undefined'){
+            this.container.insertAdjacentHTML(`beforeend`, `<ul class="search">
+            <button type=button id= "btn5" class="results">${data.results[5].title} <img src=${data.results[5].image}> </button> </div> `); 
+
+            document.getElementById("btn5").addEventListener("click",() => {
+                localStorage.setItem(`recipe`,JSON.stringify(data));
+                localStorage.setItem(`current-recipe`,JSON.stringify(5)); 
+                document.location.href = "./second-page.html";
+            }) 
+        } else {null;}
+
+        if (data.results[6] !== 'undefined'){
+            this.container.insertAdjacentHTML(`beforeend`, `<ul class="search">
+            <button type=button id= "btn6" class="results">${data.results[6].title} <img src=${data.results[6].image}> </button> </div> `); 
+
+            document.getElementById("btn6").addEventListener("click",() => {
+                localStorage.setItem(`recipe`,JSON.stringify(data));
+                localStorage.setItem(`current-recipe`,JSON.stringify(6)); 
+                document.location.href = "./second-page.html";
+            }) 
+        } else {null;}
+
+        if (data.results[7] !== 'undefined'){
+            this.container.insertAdjacentHTML(`beforeend`, `<ul class="search">
+            <button type=button id= "btn7" class="results">${data.results[7].title} <img src=${data.results[7].image}> </button> </div> `); 
+
+            document.getElementById("btn7").addEventListener("click",() => {
+                localStorage.setItem(`recipe`,JSON.stringify(data));
+                localStorage.setItem(`current-recipe`,JSON.stringify(7)); 
+                document.location.href = "./second-page.html";
+            }) 
+        } else {null;}
+
+        if (data.results[8] !== 'undefined'){
+            this.container.insertAdjacentHTML(`beforeend`, `<ul class="search">
+            <button type=button id= "btn8" class="results">${data.results[8].title} <img src=${data.results[8].image}> </button> </div> `); 
+
+            document.getElementById("btn8").addEventListener("click",() => {
+                localStorage.setItem(`recipe`,JSON.stringify(data));
+                localStorage.setItem(`current-recipe`,JSON.stringify(8)); 
+                document.location.href = "./second-page.html";
+            }) 
+        } else {null;}
+
+        if (data.results[9] !== 'undefined'){
+            this.container.insertAdjacentHTML(`beforeend`, `<ul class="search">
+            <button type=button id= "btn9" class="results">${data.results[9].title} <img src=${data.results[9].image}> </button> </div> `); 
+
+            document.getElementById("btn9").addEventListener("click",() => {
+                localStorage.setItem(`recipe`,JSON.stringify(data));
+                localStorage.setItem(`current-recipe`,JSON.stringify(9)); 
+                document.location.href = "./second-page.html";
+            }) 
+        } else {null;}
+/*
+        if (data.results[1] !== 'undefined'){
             this.container.innerHTML += 
             `<ul class="search">
-                <button type=button id= "${data.results[1].title}" class="results">${data.results[1].title} <img src=${data.results[1].image} </button>
+                <button type=button id= "btn1" class="results">${data.results[1].title} <img src=${data.results[1].image} </button>
             </div> `
-            var btn1 = document.getElementById(`${data.results[1].title}`);
+            var btn1 = document.getElementById("btn1");
             btn1.addEventListener("click",() => {
                 localStorage.setItem(`recipe`,JSON.stringify(data));
                 localStorage.setItem(`current-recipe`,JSON.stringify(1)); 
@@ -186,8 +278,11 @@ class showResults{
             })
         } else {null;}
 
-    }};
+*/
 
+
+    }};
+   
 
 
 function clear(){
@@ -220,6 +315,7 @@ button.addEventListener("click", () => {
         SR.displaySearch(data);
     })
 })
+
 search.addEventListener("keydown", function(event) {
     if (event.key === "Enter"){
         button.click();
@@ -248,7 +344,7 @@ search.addEventListener("keydown", function(event) {
         document.location.href = "./third-page.html"
         })
     })
- 
+
 //     class trivia {
 //         async randomTrivia() {
 //     // added a second key we can use if we get to more then the set number of calls on the first key. just comment one
